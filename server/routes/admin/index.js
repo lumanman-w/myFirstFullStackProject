aec42cf9d19dd0f0c2c5dcabe66515837e45b15d
lumanman <lumanman_w@foxmail.com>
2020-05-02 21:34:20 +0800

    实现分类编辑功能

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -9,8 +9,19 @@ module.exports = app => {
   })
 
   router.get('/categories', async (req, res) => {
-    const items = await Category.find().limit(10) // 创建数据
+    const items = await Category.find().limit(10) // 获取数据列表10条
     res.send(items)
   })
+
+  router.get('/categories/:id', async (req, res) => {
+    const model = await Category.findById(req.params.id) // 根据ID获取数据
+    res.send(model)
+  })
+
+  router.put('/categories/:id', async (req, res) => {
+    const model = await Category.findByIdAndUpdate(req.params.id, req.body) // 修改数据
+    res.send(model)
+  })
+
   app.use('/admin/api', router)
 }
