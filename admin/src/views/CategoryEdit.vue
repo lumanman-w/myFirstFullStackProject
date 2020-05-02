f28faef411987d6a0e1cdd49d5f61fa453aac23a
lumanman <lumanman_w@foxmail.com>
2020-05-02 22:35:11 +0800

    添加子集分类

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,6 +2,16 @@
   <div class="about">
     <h1>{{ id ? "编辑" : "新建" }}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="父级列表">
+        <el-select v-model="model.parent">
+          <el-option
+            v-for="item in parents"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -20,6 +30,7 @@ export default {
   data() {
     return {
       model: {},
+      parents: [],
     };
   },
   methods: {
@@ -39,8 +50,13 @@ export default {
       const res = await this.$http.get(`categories/${this.id}`);
       this.model = res.data;
     },
+    async fetchParents() {
+      const res = await this.$http.get(`categories`);
+      this.parents = res.data;
+    },
   },
   created() {
+    this.fetchParents();
     this.id && this.fetch();
   },
 };
--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -5,6 +5,7 @@
     </h1>
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="220"> </el-table-column>
+      <el-table-column prop="parent.name" label="上级分类"> </el-table-column>
       <el-table-column prop="name" label="分类名称"> </el-table-column>
       <el-table-column fixed="right" label="操作" width="180">
         <template slot-scope="scope">
