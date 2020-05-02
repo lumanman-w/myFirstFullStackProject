8ef39126b92c9ebaa03b3519b0eff84496fa9ebd
lumanman <lumanman_w@foxmail.com>
2020-05-03 00:33:04 +0800

    实现图片上传功能

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(require('cors')()) // 引入跨域模块
 app.use(express.json()) // 引用json模块
+app.use('/uploads', express.static(__dirname + '/uploads')) // 静态托管文件
 
 require('./plugins/db')(app) //连接数据库
 require('./routes/admin')(app)
