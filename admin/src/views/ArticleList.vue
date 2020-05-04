67d14c1da220ec62904fb435f49eebb23802439e
lumanman <lumanman_w@foxmail.com>
2020-05-04 13:36:46 +0800

    添加文章模块+富文本编辑器

---
+++ admin/src/views/ArticleList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h1>
+      分类列表
+    </h1>
+    <el-table :data="items">
+      <el-table-column prop="_id" label="ID" width="220"> </el-table-column>
+      <el-table-column prop="title" label="标题"> </el-table-column>
+      <el-table-column fixed="right" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button
+            type="primary"
+            size="small"
+            @click="$router.push(`/articles/edit/${scope.row._id}`)"
+          >
+            编辑
+          </el-button>
+          <el-button type="primary" size="small" @click="remove(scope.row)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    /* 可调用的方法 */
+    async fetch() {
+      const res = await this.$http.get(
+        "rest/articles"
+      ); /* 注意要有await,否则列表中的数据不展示 */
+      this.items = res.data;
+    },
+    async remove(row) {
+      this.$confirm(`是否删除分类"${row.title}"`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(async () => {
+          await this.$http.delete(`rest/articles/${row._id}`);
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
+          this.fetch();
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
+  },
+  created() {
+    /* 初始化时执行的方法 */
+    this.fetch();
+  },
+};
+</script>
