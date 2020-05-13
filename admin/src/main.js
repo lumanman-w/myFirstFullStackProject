87ce10ef90fc581b0d6e0b0ab3f837e17bcb6613
lumanman <lumanman_w@foxmail.com>
2020-05-13 22:03:21 +0800

    添加客户端登录验证，上传文件登录验证（上传文件接口携带token）

--- admin/src/main.js
+++ admin/src/main.js
@@ -9,6 +9,20 @@ import './style.css'
 Vue.config.productionTip = false
 Vue.prototype.$http = http /* 将http加载到vue的原型上面之后，在任意界面，都可以使用this.$http进行访问 */
 
+Vue.mixin({
+  computed: {
+    uploadUrl () {
+      return this.$http.defaults.baseUrl + '/upload'
+    }
+  },
+  methods: {
+    getAuthHeaders () {
+      return {
+        Authorization: `Bearer ${localStorage.token || ''}`
+      }
+    }
+  },
+})
 new Vue({
   router,
   render: h => h(App)
--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -25,7 +25,8 @@
             <!-- <el-input v-model="model.icon"></el-input> -->
             <el-upload
               class="avatar-uploader"
-              :action="$http.defaults.baseURL + '/upload'"
+              :action="uploadUrl"
+              :headers="getAuthHeaders()"
               :show-file-list="false"
               :on-success="afterUpload"
             >
@@ -114,7 +115,8 @@
               <el-form-item label="图标">
                 <el-upload
                   class="avatar-uploader"
-                  :action="$http.defaults.baseURL + '/upload'"
+                  :action="uploadUrl"
+                  :headers="getAuthHeaders()"
                   :show-file-list="false"
                   :on-success="(res) => $set(item, 'icon', res.url)"
                 >
